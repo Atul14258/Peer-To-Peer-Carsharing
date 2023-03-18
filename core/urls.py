7a484de0b4959c166a9350c6204214bdcb7f9f0a
lalit <lalitjajoo_ce_2021@ltce.in>
2023-03-18 19:43:13 +0530

    Merge remote-tracking branch 'origin/main'

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('car_single',views.car_single,name='car_single'),
     path('contact',views.contact,name='contact'),
     path('car_book',views.car_book,name='car_book'),
+    path('pricing',views.pricing,name='pricing'),
 
 
 ]
